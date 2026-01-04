a4fbcea889273481828645ec3503a6afef3b90bd
Avinash Singh <avinashsngh21@gmail.com>
2026-01-04 13:31:09 -0500

    surface_subroutince added

--- __init__.py
+++ __init__.py
@@ -10,13 +10,27 @@ Author: Avinash Kumar Singh
 from .rays import Ray, normalize
 from .rays import create_ray_bundle_parallel, create_ray_fan_meridional, create_ray_grid
 
+from .surfaces import Surface, SphericalSurface, ConicSurface, AsphereSurface
+from .surfaces import SurfaceType
+from .surfaces import create_flat_surface, create_mirror, create_parabolic_mirror
+
 __version__ = "0.1.0"
 __author__ = "Avinash Kumar Singh"
 
 __all__ = [
+    # Rays
     "Ray",
     "normalize",
     "create_ray_bundle_parallel",
     "create_ray_fan_meridional",
     "create_ray_grid",
+    # Surfaces
+    "Surface",
+    "SphericalSurface",
+    "ConicSurface",
+    "AsphereSurface",
+    "SurfaceType",
+    "create_flat_surface",
+    "create_mirror",
+    "create_parabolic_mirror",
 ]
